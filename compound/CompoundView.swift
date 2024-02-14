6a4523b2719a4b4ca739cbfcd322202e41fcf04d
Daniel Jones <messyjones@gmail.com>
2024-02-13 20:30:39 -0800

    constraints are starting to look better

--- compound/CompoundView.swift
+++ compound/CompoundView.swift
@@ -8,7 +8,8 @@
 import UIKit
 
 class CompoundView: UIView {
-    var containerView: ExpandableView!
+    var containerView: UIView!
+    var assetViews: [ExpandableView] = []
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -21,20 +22,41 @@ class CompoundView: UIView {
     }
     
     private func setContainerView() {
-        containerView = ExpandableView()
+        containerView = UIView()
         self.containerView.backgroundColor = ColorHelper.color(fromHex: "#1D2833")
         self.layer.borderWidth = 0.25
         self.layer.borderColor = ColorHelper.color(fromHex: "#F9FAFB26").cgColor
         
         self.addSubview(containerView)
         containerView.translatesAutoresizingMaskIntoConstraints = false
-        containerView.heightConstraint = containerView.heightAnchor.constraint(equalToConstant: 100)
         NSLayoutConstraint.activate([
             containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
             containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
-            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 60),
-            containerView.heightConstraint!
+            containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 60)
         ])
+    }
+    
+    func add(_ assets: [Asset]) {
+        for (index, asset) in assets.enumerated() {
+            let expandableView = ExpandableView()
+            expandableView.translatesAutoresizingMaskIntoConstraints = false
+            containerView.addSubview(expandableView)
+            expandableView.backgroundColor = .red
+            assetViews.append(expandableView)
+            
+            var topAnchor = expandableView.topAnchor.constraint(equalTo: containerView.topAnchor)
+            if index > 0 {
+                let aboveView = assetViews[index - 1]
+                topAnchor = expandableView.topAnchor.constraint(equalTo: aboveView.bottomAnchor)
+            }
+            
+            NSLayoutConstraint.activate([
+                expandableView.leadingAnchor.constraint(equalTo: self.containerView.leadingAnchor),
+                expandableView.trailingAnchor.constraint(equalTo: self.containerView.trailingAnchor),
+                topAnchor,
+                expandableView.heightAnchor.constraint(equalToConstant: 100)
+            ])
+        }
     }
     
     
@@ -90,3 +112,14 @@ class ExpandableView: UIView {
         }
     }
 }
+
+//
+//self.addSubview(containerView)
+//containerView.translatesAutoresizingMaskIntoConstraints = false
+//containerView.heightConstraint = containerView.heightAnchor.constraint(equalToConstant: 100)
+//NSLayoutConstraint.activate([
+//    containerView.leadingAnchor.constraint(equalTo: self.leadingAnchor),
+//    containerView.trailingAnchor.constraint(equalTo: self.trailingAnchor),
+//    containerView.topAnchor.constraint(equalTo: self.topAnchor, constant: 60),
+//    containerView.heightConstraint!
+//])
--- compound/ViewController.swift
+++ compound/ViewController.swift
@@ -25,10 +25,33 @@ class ViewController: UIViewController {
             do {
                 let assets = try await DataStore().fetchAssetsFromAPI()
                 if let compoundView = self.view as? CompoundView {
-                    
+                    compoundView.add(assets)
+                    //not the best way to download images in a for loop.
+                    //buit focusing time on functionality right now
+                    for (index, asset) in assets.enumerated() {
+                        downloadImage(from: asset.image, index: index)
+                    }
                 }
             } catch {
                 print(error)
+            }
+        }
+    }
+    
+    private func getData(from url: URL, completion: @escaping (Data?, URLResponse?, Error?) -> ()) {
+        URLSession.shared.dataTask(with: url, completionHandler: completion).resume()
+    }
+    
+    private func downloadImage(from urlStr: String, index: Int) {
+        let url = URL(string: urlStr)
+        print("Download Started")
+        getData(from: url!) { data, response, error in
+            guard let data = data, error == nil else { return }
+            // always update the UI from the main thread
+            DispatchQueue.main.async() { [weak self] in
+                if let compoundView = self?.view as? CompoundView {
+                    compoundView.assetViews[index].logoImg.image = UIImage(data: data)
+                }
             }
         }
     }
