7fa6b8aaf592b82f7a6792ef8acd1842f91ca22a
Daniel Jones <messyjones@gmail.com>
2024-02-13 18:46:29 -0800

    starting commit. The programmatic view loads

---
+++ compound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  compound
+//
+//  Created by Daniel Jones on 2/13/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+
+}
+
